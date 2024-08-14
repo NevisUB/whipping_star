8e763ad95b5a816d72918cb2d56e00fd425ac691
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2024-08-14 15:45:08 -0500

    fixed boost in CMAKe and setp script for SL7 containter

--- setup.sh
+++ setup.sh
@@ -1,13 +1,25 @@
-source /grid/fermiapp/products/uboone/setup_uboone.sh; 
-setup git v2_15_1; 
-setup gitflow v1_11_0; 
-setup mrb v1_16_02;
-#setup root v6_10_04d -q e14:nu:prof; #Old SL6  
-setup root v6_12_04e -q e15:prof; 
-setup cmake v3_11_4;
-setup eigen v3_3_4a
-#setup hdf5 v1_10_5 -qe19// Now manually compiled with MPI support
-setup gcc v6_4_0 #
+source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone_mcc9.sh
+
+echo "Setting up ninja v1_8_4"
+setup ninja v1_8_2
+echo "Setting up uboonecode v08_00_00_64"
+setup uboonecode v08_00_00_64 -qe17:prof
+echo "Setting up cmake v3_14_3"
+setup cmake v3_14_3
+echo "Setting up gcc v7_3_0"
+setup gcc v7_3_0
+setup root v6_12_04e -q e15:prof
+echo "Setting up GSL"
+setup gsl v2_4 -q prof
+echo "Setting up Eigen"
+setup eigen v3_3_4a;
+setup boost v1_66_0 -qe15:prof
+#setup boost v1_82_0 -qe20:prof
+
+cd /uboone/app/users/markrl
+
+
+
 
 me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $me
